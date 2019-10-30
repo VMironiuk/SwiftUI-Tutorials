b3b06a855fc591d4aeb86082afd08eeea02f7b52
Vladimir Mironiuk <vladimironiuk@gmail.com>
2019-10-30 14:07:19 +0200

    SwiftUI Essentials > Creating and Combining Views > Use UIKit and SwiftUI Views Together

---
+++ Landmarks/Landmarks/MapView.swift
@@ -0,0 +1,29 @@
+//
+//  MapView.swift
+//  Landmarks
+//
+//  Created by Vladimir Mironiuk on 30.10.2019.
+//  Copyright © 2019 Vladimir Mironiuk. All rights reserved.
+//
+
+import SwiftUI
+import MapKit
+
+struct MapView: UIViewRepresentable {
+  func makeUIView(context: Context) -> MKMapView {
+    MKMapView(frame: .zero)
+  }
+  
+  func updateUIView(_ uiView: MKMapView, context: Context) {
+    let coordinate = CLLocationCoordinate2D(latitude: 34.011286, longitude: -116.166868)
+    let span = MKCoordinateSpan(latitudeDelta: 2.0, longitudeDelta: 2.0)
+    let region = MKCoordinateRegion(center: coordinate, span: span)
+    uiView.setRegion(region, animated: true)
+  }
+}
+
+struct MapView_Previews: PreviewProvider {
+  static var previews: some View {
+    MapView()
+  }
+}
