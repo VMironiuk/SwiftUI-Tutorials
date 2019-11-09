f05fcdbc7ef24c7e2e5437facd918f49cb362eb9
Vladimir Mironiuk <vladimironiuk@gmail.com>
2019-11-09 12:01:45 +0200

    SwiftUI Essentials > Building Lists and Navigation > Generatirn Previews Dynamically

--- Building Lists and Navigation/Landmarks/Landmarks/LandmarkList.swift
+++ Building Lists and Navigation/Landmarks/Landmarks/LandmarkList.swift
@@ -23,6 +23,10 @@ struct LandmarkList: View {
 
 struct LandmarkList_Previews: PreviewProvider {
   static var previews: some View {
-    LandmarkList()
+    ForEach(["iPhone SE", "iPhone 11 Pro"], id: \.self) { displayName in
+      LandmarkList()
+        .previewDevice(PreviewDevice(rawValue: displayName))
+        .previewDisplayName(displayName)
+    }
   }
 }
