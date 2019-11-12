d1bc42a2eb506d5d38b143b13158bd6105bd8ab3
Vladimir Mironiuk <vladimironiuk@gmail.com>
2019-11-12 10:48:43 +0200

    SwiftUI Essentials > Handling User Input > Filter the List View

--- Handling User Input/Landmarks/Landmarks/LandmarkList.swift
+++ Handling User Input/Landmarks/Landmarks/LandmarkList.swift
@@ -1,31 +1,35 @@
 /*
-See LICENSE folder for this sample’s licensing information.
-
-Abstract:
-A view showing a list of landmarks.
-*/
+ See LICENSE folder for this sample’s licensing information.
+ 
+ Abstract:
+ A view showing a list of landmarks.
+ */
 
 import SwiftUI
 
 struct LandmarkList: View {
-    var body: some View {
-        NavigationView {
-            List(landmarkData) { landmark in
-                NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
-                    LandmarkRow(landmark: landmark)
-                }
-            }
-            .navigationBarTitle(Text("Landmarks"))
+  @State var showFavoritesOnly = false
+  
+  var body: some View {
+    NavigationView {
+      List(landmarkData) { landmark in
+        if !self.showFavoritesOnly || landmark.isFavorite {
+          NavigationLink(destination: LandmarkDetail(landmark: landmark)) {
+            LandmarkRow(landmark: landmark)
+          }
         }
+      }
+      .navigationBarTitle(Text("Landmarks"))
     }
+  }
 }
 
 struct LandmarkList_Previews: PreviewProvider {
-    static var previews: some View {
-        ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
-            LandmarkList()
-                .previewDevice(PreviewDevice(rawValue: deviceName))
-                .previewDisplayName(deviceName)
-        }
+  static var previews: some View {
+    ForEach(["iPhone SE", "iPhone XS Max"], id: \.self) { deviceName in
+      LandmarkList()
+        .previewDevice(PreviewDevice(rawValue: deviceName))
+        .previewDisplayName(deviceName)
     }
+  }
 }
