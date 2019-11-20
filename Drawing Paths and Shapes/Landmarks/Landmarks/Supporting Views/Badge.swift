b132cae02446f03931dfc096af052cd9dcbac2a0
Vladimir Mironiuk <vladimironiuk@gmail.com>
2019-11-20 10:06:38 +0200

    Drawing and Animation > Drawing Paths and Shapes > Crate a Badge View

---
+++ Drawing Paths and Shapes/Landmarks/Landmarks/Supporting Views/Badge.swift
@@ -0,0 +1,21 @@
+//
+//  Badge.swift
+//  Landmarks
+//
+//  Created by Vladimir Mironiuk on 20.11.2019.
+//  Copyright © 2019 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct Badge: View {
+  var body: some View {
+    Text("Badge")
+  }
+}
+
+struct Badge_Previews: PreviewProvider {
+  static var previews: some View {
+    Badge()
+  }
+}
