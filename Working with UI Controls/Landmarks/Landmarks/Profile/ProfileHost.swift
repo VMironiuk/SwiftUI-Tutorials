673bdb502243163b9eed5903cdba23535e868298
Vladimir Mironiuk <vladimironiuk@gmail.com>
2019-12-24 10:29:28 +0200

    App Design and Layout > Working with UI Controls > Add an Edit Mode

--- Working with UI Controls/Landmarks/Landmarks/Profile/ProfileHost.swift
+++ Working with UI Controls/Landmarks/Landmarks/Profile/ProfileHost.swift
@@ -9,17 +9,29 @@
 import SwiftUI
 
 struct ProfileHost: View {
+  @Environment(\.editMode) var mode
+  @EnvironmentObject var userData: UserData
   @State var drafrtProfile = Profile.default
   
   var body: some View {
     VStack(alignment: .leading, spacing: 20) {
-      ProfileSummary(profile: self.drafrtProfile)
+      HStack {
+        Spacer()
+        EditButton()
+      }
+      if self.mode?.wrappedValue == .inactive {
+        ProfileSummary(profile: self.drafrtProfile)
+      }
+      else {
+        Text("Profile Editor")
+      }
     }
+    .padding()
   }
 }
 
 struct ProfileHost_Previews: PreviewProvider {
   static var previews: some View {
-    ProfileHost()
+    ProfileHost().environmentObject(UserData())
   }
 }
