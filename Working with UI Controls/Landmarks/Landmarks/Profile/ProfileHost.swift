94f9b7ebc45f4abcaf12670c8d3f57c4a19e8e35
Vladimir Mironiuk <vladimironiuk@gmail.com>
2019-12-25 09:25:23 +0200

    App Design and Layout > Working with UI Controls > Delay Edit Propagation

--- Working with UI Controls/Landmarks/Landmarks/Profile/ProfileHost.swift
+++ Working with UI Controls/Landmarks/Landmarks/Profile/ProfileHost.swift
@@ -11,19 +11,33 @@ import SwiftUI
 struct ProfileHost: View {
   @Environment(\.editMode) var mode
   @EnvironmentObject var userData: UserData
-  @State var drafrtProfile = Profile.default
+  @State var draftProfile = Profile.default
   
   var body: some View {
     VStack(alignment: .leading, spacing: 20) {
       HStack {
+        if self.mode?.wrappedValue == .active {
+          Button("Cancel") {
+            self.draftProfile = self.userData.profile
+            self.mode?.animation().wrappedValue = .inactive
+          }
+        }
+        
         Spacer()
+        
         EditButton()
       }
       if self.mode?.wrappedValue == .inactive {
-        ProfileSummary(profile: self.drafrtProfile)
+        ProfileSummary(profile: self.draftProfile)
       }
       else {
-        ProfileEditor(profile: $drafrtProfile)
+        ProfileEditor(profile: $draftProfile)
+          .onAppear {
+            self.draftProfile = self.userData.profile
+        }
+        .onDisappear {
+          self.userData.profile = self.draftProfile
+        }
       }
     }
     .padding()
