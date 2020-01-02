3f58036f63c0f7f242520ef117f8f1c13173d4d3
Vladimir Mironiuk <vladimironiuk@gmail.com>
2020-01-02 11:59:19 +0200

    Framework Integration > Interfacing with UIKit > Create the View Controller's Data Source

--- Interfacing with UIKit/Landmarks/Landmarks/Controllers/PageViewController.swift
+++ Interfacing with UIKit/Landmarks/Landmarks/Controllers/PageViewController.swift
@@ -12,10 +12,15 @@ import UIKit
 struct PageViewController: UIViewControllerRepresentable {
   var controllers: [UIViewController]
   
+  func makeCoordinator() -> Coordinator {
+    Coordinator(self)
+  }
+  
   func makeUIViewController(context: Context) -> UIPageViewController {
     let pageViewController = UIPageViewController(
       transitionStyle: .scroll,
       navigationOrientation: .horizontal)
+    pageViewController.dataSource = context.coordinator
     
     return pageViewController
   }
@@ -25,5 +30,37 @@ struct PageViewController: UIViewControllerRepresentable {
       [self.controllers[0]],
       direction: .forward,
       animated: true)
+  }
+  
+  class Coordinator: NSObject, UIPageViewControllerDataSource {
+    var parent: PageViewController
+    
+    init(_ pageViewController: PageViewController) {
+      self.parent = pageViewController
+    }
+    
+    func pageViewController(
+      _ pageViewController: UIPageViewController,
+      viewControllerBefore viewController: UIViewController) -> UIViewController? {
+      guard let index = self.parent.controllers.firstIndex(of: viewController) else {
+        return nil
+      }
+      if index == 0 {
+        return self.parent.controllers.last
+      }
+      return self.parent.controllers[index - 1 ]
+    }
+    
+    func pageViewController(
+      _ pageViewController: UIPageViewController,
+      viewControllerAfter viewController: UIViewController) -> UIViewController? {
+      guard let index = self.parent.controllers.firstIndex(of: viewController) else {
+        return nil
+      }
+      if index + 1 == self.parent.controllers.count {
+        return self.parent.controllers.first
+      }
+      return self.parent.controllers[index + 1]
+    }
   }
 }
