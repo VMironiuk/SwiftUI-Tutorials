a62978e6a06c9efbaa9a7a49d1ad929b6ae94c49
Vladimir Mironiuk <vladimironiuk@gmail.com>
2020-01-11 07:44:54 +0200

    Framework Integration > Creating a watchOS app > Add a watchOS target

---
+++ Creating a watchOS App/Landmarks/WatchLandmarks Extension/HostingController.swift
@@ -0,0 +1,17 @@
+//
+//  HostingController.swift
+//  WatchLandmarks Extension
+//
+//  Created by Vladimir Mironiuk on 11.01.2020.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import WatchKit
+import Foundation
+import SwiftUI
+
+class HostingController: WKHostingController<ContentView> {
+    override var body: ContentView {
+        return ContentView()
+    }
+}
---
+++ Creating a watchOS App/Landmarks/WatchLandmarks Extension/NotificationView.swift
@@ -0,0 +1,21 @@
+//
+//  NotificationView.swift
+//  WatchLandmarks Extension
+//
+//  Created by Vladimir Mironiuk on 11.01.2020.
+//  Copyright © 2020 Apple. All rights reserved.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
